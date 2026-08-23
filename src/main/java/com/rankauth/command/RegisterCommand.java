12c50bb6898038349652b43b831f124a6c70f871
Yukile394 <muhamedmuhet@gmail.com>
2026-08-23 10:46:50 +0300

    Add ConfigManager to RegisterCommand constructor

--- src/main/java/com/rankauth/command/RegisterCommand.java
+++ src/main/java/com/rankauth/command/RegisterCommand.java
@@ -1,7 +1,7 @@
 package com.rankauth.command;
 
 import com.rankauth.auth.AuthManager;
-import org.bukkit.ChatColor;
+import com.rankauth.config.ConfigManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,9 +10,11 @@ import org.bukkit.entity.Player;
 public final class RegisterCommand implements CommandExecutor {
 
     private final AuthManager authManager;
+    private final ConfigManager config;
 
-    public RegisterCommand(AuthManager authManager) {
+    public RegisterCommand(AuthManager authManager, ConfigManager config) {
         this.authManager = authManager;
+        this.config = config;
     }
 
     @Override
@@ -22,7 +24,7 @@ public final class RegisterCommand implements CommandExecutor {
             return true;
         }
         if (args.length != 2) {
-            player.sendMessage(ChatColor.YELLOW + "Kullanım: /register <şifre> <şifre>");
+            player.sendMessage(config.message("register-usage"));
             return true;
         }
         authManager.handleRegisterCommand(player, args[0], args[1]);
